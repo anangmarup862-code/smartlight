3248f52aeadafe9dd87f00869acb4ca826e394d6
anangmarup862-code <anangmarup862@gmail.com>
2025-10-01 15:49:28 +0700

    Add files via upload

---
+++ smartlighting.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, send_from_directory # pyright: ignore[reportMissingImports]
+import os
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+# === ROUTES ===
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Route untuk memastikan JSON bisa diakses
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory(app.static_folder, filename)
+
+# Jalankan server
+if __name__ == "__main__":
+    # host=0.0.0.0 supaya bisa diakses dari luar (hosting / LAN)
+    app.run(host="0.0.0.0", port=5000, debug=False)
